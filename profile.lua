7d21107171ab62f24ab0b61a4736504782091b57
Mew903 <mew903@users.noreply.github.com>
2021-05-23 04:19:58 -0400

    Update profile.lua
    + Added remote function & remote event for relaying information to clients.
    + Added 'Listeners' which act as whitelisting a Player to access their profile data / others data.
    + Added 'UserInputKeys' which act as keys designated to be assigned as strings from user input

--- profile.lua
+++ profile.lua
@@ -1,6 +1,8 @@
 -- RbxDb.Profile
 -- mew903, 2021
 
+local TextService = game:GetService('TextService');
+
 local Database = require(script.Parent);
 local ProfileDatabase = Database('RbxDb_Profile');
 local SessionDatabase = Database('RbxDb_Session');
@@ -8,14 +10,96 @@ local SessionDatabase = Database('RbxDb_Session');
 -- flags
 __DEBUG__ = true;
 __VERBOSE__ = true;
+__RESTRICT_GET__ = false;
 __TEST_PROFILE__ = false;
 __AUTO_RECONCILE__ = true;
 
 local Profile = { }; do
+	Profile.UserInputKeys = { };
 	Profile.__index = Profile;
 	
+	local function copy(Input)
+		local newProfile = { };
+
+		for key, value in next, Input do
+			if typeof(value) == 'table' then
+				newProfile[key] = copy(value);
+			else
+				newProfile[key] = value;
+			end;
+		end;
+
+		return newProfile;
+	end;
+
+	local function key(Player)
+		return (__TEST_PROFILE__ and 'TEST_' or 'P_') .. Player.UserId;
+	end;
+	
+	local function reconcile(Data, Template, ProfileKey, Byref, Path)
+		local path = Path or '';
+		
+		for key, value in next, Data do
+			local templateValue = Template[key];
+			
+			if typeof(templateValue) == 'nil' then
+				Data[key] = nil;
+				
+				table.insert(Byref, {
+					old = value;
+					new = Data[key];
+					key = path .. key;
+					source = ProfileKey;
+				});
+			elseif typeof(templateValue) == 'function' then
+				templateValue(Data, value);
+
+				table.insert(Byref, {
+					old = value;
+					new = Data[key];
+					key = path .. key;
+					source = ProfileKey;
+				});
+			elseif typeof(templateValue) == 'table' and typeof(value) == 'table' then
+				reconcile(value, templateValue, ProfileKey, Byref, key .. '/');
+			end;
+		end;
+
+		for key, value in next, Template do
+			if Data[key] == nil then
+				local newValue;
+				
+				if typeof(value) == 'function' then
+					newValue = value(Data);
+				elseif typeof(value) == 'table' then
+					newValue = copy(value);
+				else
+					newValue = value;
+				end;
+				
+				Data[key] = newValue;
+
+				table.insert(Byref, {
+					old = nil;
+					new = newValue;
+					key = path .. key;
+					source = '<template>';
+				});
+			end;
+		end;
+		
+		return Byref;
+	end;
+	
+	function Profile.AddListener(self, Listener)
+		assert(Listener and typeof(Listener) == 'Instance' and Listener:IsA('Player'), 'bad argument #1 must be a Player');
+		assert(not table.find(self._listeners, Listener), string.format('bad argument #1 listener already exists for profile `%s`', self._key));
+		
+		table.insert(self._listeners, Listener);
+	end;
+	
 	function Profile.Get(self, Key)
-		assert(Key and typeof(Key) == 'string', 'Invalid argument to #1 (string expected)');
+		assert(Key and typeof(Key) == 'string', 'bad argument #1 must be string');
 		
 		if __DEBUG__ then
 			ProfileDatabase:Out(string.format('GET `%s`{%s}', self._key, Key or nil));
@@ -24,44 +108,30 @@ local Profile = { }; do
 		return self._data[Key];
 	end;
 	
+	function Profile.HookToChange(self, Key, Callback)
+		assert(Key and typeof(Key) == 'string', 'bad argument #1 must be string');
+		assert(Callback and typeof(Callback) == 'function', 'bad argument #2 must be function');
+		
+		if not self._changehooks[Key] then
+			self._changehooks[Key] = { };
+		end;
+		
+		table.insert(self._changehooks[Key], Callback);
+		
+		return function()
+			table.remove(self._changehooks[Key], table.find(self._changehooks[Key], Callback));
+		end;
+	end;
+	
 	function Profile.Reconcile(self)
-		local reconciled = { };
-		
-		for key, value in next, self._data do
-			if typeof(self._template[key]) == 'function' then
-				local result = self._template[key](self._data, value);
-				
-				if result == -1 then
-					self._data[key] = nil;
-				end;
-				
-				table.insert(reconciled, {
-					key = key;
-					old = value;
-					source = self._key;
-					new = self._data[key];
-				});
-			end;
-		end;
-		
-		for key, value in next, self._template do
-			if self._data[key] == nil then
-				self._data[key] = typeof(value) == 'function' and value(self._data) or value;
-				
-				table.insert(reconciled, {
-					key = key;
-					old = nil;
-					source = 'template';
-					new = self._data[key];
-				});
-			end;
-		end;
+		local reconciled = reconcile(self._data, self._template, self._key, { });
 		
 		if #reconciled > 0 and __VERBOSE__ then
 			ProfileDatabase:Out(string.format('Profile reconciled for: %s {KEY=`%s`}', self._player.Name, self._key));
 			
 			for _, attribute in next, reconciled do
-				ProfileDatabase:Out(string.format('\tATTR=`%s`{VALUE=`%s` -> `%s`}', attribute.key, attribute.old or 'nil', attribute.new or 'nil'));
+				ProfileDatabase:Out(string.format('\tATTR=`%s`{VALUE=`%s` -> `%s`}', 
+					attribute.key, tostring(attribute.old) or 'nil', tostring(attribute.new) or 'nil'));
 			end;
 		end;
 		
@@ -90,33 +160,82 @@ local Profile = { }; do
 		return endSession.Queue;
 	end;
 	
+	function Profile.RemoveListeners(self, Listener)
+		assert(Listener, 'bad argument #1 cannot be nil');
+		assert(Listener ~= self._player, 'bad argument #1 cannot be profile owner');
+		assert(typeof(Listener) == 'Instance' and Listener:IsA('Player'), 'bad argument #1 must be a Player');
+		
+		local index = table.find(self._listeners, Listener);
+		
+		if index then
+			return true, table.remove(self._listeners, index);
+		end;
+		
+		return false;
+	end;
+	
 	function Profile.Set(self, Key, Value)
-		assert(Key and typeof(Key) == 'string', 'Invalid argument to #1 (string expected)');
-		assert(Value, 'Invalid argument to #2 (Anything but nil expected)');
+		assert(Key and typeof(Key) == 'string', 'bad argument to #1 must be string');
+		assert(Value, 'bad argument to #2 cannot be nil');
 		
 		if __VERBOSE__ then
 			ProfileDatabase:Out(string.format('SET `%s`{%s=%s}', self._key, Key, tostring(Value)));
 		end;
 		
-		self._data[Key] = Value;
-	end;
-
-	local function copy(Input)
-		local newProfile = { };
-
-		for key, value in next, Input do
-			if typeof(value) == 'table' then
-				newProfile[key] = copy(value);
-			else
-				newProfile[key] = value;
+		if Profile.UserInputKeys[Key] then
+			assert(typeof(Value) == 'string', 'bad argument to #2 value must be string');
+			
+			local function filter()
+				local filterResult = TextService:FilterStringAsync(Value, self._player.UserId, Enum.TextFilterContext.PublicChat);
+				local filteredValue = filterResult:GetNonChatStringForBroadcastAsync();
+				
+				Value = filteredValue;
+			end;
+			
+			if not xpcall(filter, warn) then
+				return false;
+			elseif typeof(Profile.UserInputKeys[Key]) == 'function' then
+				Value = Profile.UserInputKeys[Key](Value);
+				
+				if not Value then
+					return false;
+				end;
 			end;
 		end;
-
-		return newProfile;
+		
+		self._data[Key] = Value;
+		
+		if self._changehooks[Key] then
+			for _, callback in next, self._changehooks[Key] do
+				coroutine.wrap(callback)(Value);
+			end;
+		end;
+		
+		for _, listener in next, self._listeners do
+			self._onchange:FireClient(listener, Key, Value);
+		end;
+		
+		return true;
 	end;
-
-	local function key(Player)
-		return (__TEST_PROFILE__ and 'TEST_' or 'P_') .. Player.UserId;
+	
+	local restrictCallback;
+	
+	local function restrict(Player)
+		if restrictCallback then
+			return restrictCallback(Player);
+		else
+			return Player:Kick('Restricted access attempt');
+		end;
+	end;
+	
+	function Profile.setUserInputKey(Key, Callback)
+		Profile.UserInputKeys[Key] = Callback or true;
+	end;
+	
+	function Profile.setRestrictCallback(Callback)
+		assert(Callback and typeof(Callback) == 'function', 'bad argument #1 must be a function');
+		
+		restrictCallback = Callback;
 	end;
 	
 	function Profile.new(Player, Template)
@@ -157,7 +276,49 @@ local Profile = { }; do
 			_data = profileData;
 			_key = playerDataKey;
 			_template = Template;
+			
+			_changehooks = { };
+			_listeners = { Player };
+			_onchange = Instance.new('RemoteEvent');
 		}, Profile);
+		
+		local onChange = profile._onchange; do
+			onChange.Name = 'ProfileChanged';
+			onChange.Parent = Player;
+		end;
+		
+		local access = Instance.new('RemoteFunction'); do
+			access.OnServerInvoke = function(Invoker, RequestType, ...)
+				local requestType = string.lower(RequestType);
+				
+				if requestType == 'set' then
+					if Invoker ~= Player then
+						return restrict(Invoker);
+					end;
+					
+					local key, value = ...;
+					assert(key and typeof(key) == 'string', 'bad argument #1 string expected');
+					assert(value, 'bad argument #2 anything except nil expected');
+					
+					return profile:Set(key, value);
+				elseif requestType == 'get' then
+					if __RESTRICT_GET__ and Invoker ~= Player then
+						return restrict(Invoker);
+					end;
+					
+					local response = { };
+					
+					for _, key in next, { ... } do
+						response[key] = profile:Get(key);
+					end;
+					
+					return response;
+				end;
+			end;
+			
+			access.Name = 'ProfileRequest';
+			access.Parent = Player;
+		end;
 
 		if __AUTO_RECONCILE__ and not isNewProfile then
 			profile:Reconcile();
