100aa3679f6f8f802833362d7150305424dbea67
Mew903 <mew903@users.noreply.github.com>
2024-06-09 22:44:21 -0400

    Update init.lua
    better

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 -- RbxDb
--- mew903, 2024
+-- mew903, 2021 - 2024
 assert(game:GetService('RunService'):IsServer(), 'RbxDb can only be used in Server Scripts')
 
 -- TODO: rollbacks using memory store service
@@ -7,12 +7,21 @@ assert(game:GetService('RunService'):IsServer(), 'RbxDb can only be used in Serv
 -- debug flags
 local __DEBUG__ = true -- turns all debug flags on
 local __PING__ = false -- ping when a new connection is made
-local __VERBOSE__ = false -- print debug info
-local __WARNINGS__ = true -- warn for misuse / code smell
+local __VERBOSE__ = false -- print debug info & warnings
+local __WARNINGS__ = true -- throws warnings for misuse / code smell
 
 -- usage flags
 local DEFAULT_SCOPE = 'global' -- roblox default
+
+-- seconds to wait between requests for the same key
+local KEY_REQUEST_THROTTLE = 6
+
+-- max # of retries before fallback actions take place
 local MAX_REQUEST_RETRIES = 10
+
+-- time format string for prints and warns
+-- non-americans will likely want: TIME_FORMAT = 'YYYY-MM-DD HH24:MI:SS'
+local TIME_FORMAT = 'YYYY-MM-DD HH12:MI:SS XM'
 
 -- types
 type map<K, V> = { [K]: V }
@@ -23,8 +32,8 @@ type weakmap<K, V> = typeof(setmetatable({ } :: map<K, V>, { } :: {
   __mode: string,
 }))
 
-export type key = number | string
-export type data = number | string | map<key, data>
+export type key = string
+export type data = boolean | number | string | map<key | number, data>
 export type version = number | string
 
 -- callbacks
@@ -59,35 +68,45 @@ type RbxDbImpl = {
 -- "classes"
 export type RbxDb = typeof(setmetatable({ } :: {
   datastore: DataStore,
-  keys: weakmap<string, number>,
-  locked: weakmap<string, boolean>,
+  keys: weakmap<key, number>,
+  locked: weakmap<key, boolean>,
   name: string,
   ordered: boolean,
   scope: string,
-  print: <A...>(A...) -> (),
-  warn: <A...>(A...) -> (),
+  print: <A...>(unknown, A...) -> (),
+  wait: (key) -> number,
+  warn: <A...>(unknown, A...) -> (),
 }, { } :: RbxDbImpl))
 
--- constants
 -- you can remove these
 local assert = assert
 local print = print
 local select = select
 local setmetatable = setmetatable
 local tick = tick
-local tostring = tostring
 local xpcall = xpcall
 local warn = warn
+
 -- you can replace these
 local clock = os.clock
-local empty = { }
+local find = table.find
 local floor = math.floor
+local gsub = string.gsub
 local pack = table.pack
+local remove = table.remove
 local resume = coroutine.resume
 local thread = coroutine.create
 local wait = task.wait
+
 -- error messages
-local ORDERED_INT_ONLY = "can only store postive integers in OrderedDataStore"
+local E_ORDERED_INT_ONLY = 'Can only store postive integers in OrderedDataStore'
+-- warning messages
+local W_ORDERED_SET_VERS = 'OrderedDataStore does not support versioning, passing null to callback'
+-- constants
+local EMPTY = { }
+local NULL_VALUE = '__RBXDB_NULL__'
+-- volatiles
+local requests = 0
 
 -- code
 local Players = game:GetService('Players') 
@@ -96,9 +115,27 @@ local DataStoreService = game:GetService('DataStoreService')
 -- TODO: cache wrapper, rollbacks
 -- local MemoryStoreService = game:GetService('MemoryStoreService')
 
+local function isInt(n: unknown): boolean
+  return type(n) == 'number' and floor(n) == n
+end
+
+local function round(n: number, d: number): number
+  return floor(n * 10 ^ d) / 10 ^ d
+end
+
+local function throttle(tries: number?): number
+  return wait((tries or 1) * (60 / (60 + (#Players:GetPlayers() * 10))))
+end
+
+local function wrapNull<T, R...>(value: T, ...: R...): (T | typeof(NULL_VALUE), R...)
+  return type(value) ~= nil and value or NULL_VALUE, ...
+end
+
 if __DEBUG__ then
   __PING__ = true
   __VERBOSE__ = true
+  __WARNINGS__ = true
+elseif __VERBOSE__ then
   __WARNINGS__ = true
 end
 
@@ -110,44 +147,48 @@ local function datastore(name: string, scope: string, ordered: boolean?, opt: Da
 end
 
 local function deleteAsync(source: RbxDb, key: key): data?
-  return source.datastore:RemoveAsync(tostring(key))
-end
-
-local function isInt(n: unknown): boolean
-  return type(n) == 'number' and floor(n) == n
-end
-
-local function request<A..., R...>(f: async<A..., R...>, source: RbxDb, key: key, retry: boolean, ...: A...): R...
-  local reponse
-  local tries = 0
-  repeat
-    tries += 1
-    wait(tries * (60 / (60 + (#Players:GetPlayers() * 10))))
-    reponse = { select(2, xpcall(f, source.warn, source, key, ...)) }
-  until not retry or tries == MAX_REQUEST_RETRIES or #reponse > 0
-  if tries == MAX_REQUEST_RETRIES then
-    -- TODO: use memory service for request fallback queue
-  end
-  return unpack(reponse)
-end
-
-local function round(n: number, d: number): number
-  return floor(n * 10 ^ d) / 10 ^ d
+  return wrapNull(source.datastore:RemoveAsync(key))
 end
 
 local function fetchAsync(source: RbxDb, key: key, opt: DataStoreGetOptions?): data?
-  return source.datastore:GetAsync(tostring(key), opt)
+  return wrapNull(source.datastore:GetAsync(key, opt))
+end
+
+local function request<A..., R...>(f: async<A..., R...>, source: RbxDb, key: key, retry: boolean, ...: A...): R...
+  source.wait(key) -- wait for key to free
+  source.keys[key] = tick()
+  requests += 1
+  local status, response
+  local tries = 0
+  repeat
+    tries += 1
+    throttle(tries)
+    response = { select(2, xpcall(f, source.warn, source, key, ...)) }
+  until not retry or tries == MAX_REQUEST_RETRIES or #response > 0
+  -- TODO: use memory service for request fallback queue
+  if tries == MAX_REQUEST_RETRIES then
+    -- woops
+  else
+    repeat
+      local null = find(response, NULL_VALUE)
+      if null then
+        response[null] = nil
+      end
+    until not null
+  end
+  requests -= 1
+  return unpack(response)
 end
 
 local function setAsync(source: RbxDb, key: key, value: data, uids: array<number>?, opt: DataStoreSetOptions?): string
   if source.ordered then
-    return source.datastore:SetAsync(tostring(key), value, nil, opt)
+    return wrapNull(source.datastore:SetAsync(key, value, nil, opt))
   end
-  return source.datastore:SetAsync(tostring(key), value, uids or empty, opt)
+  return wrapNull(source.datastore:SetAsync(key, value, uids or EMPTY, opt))
 end
 
 local function updateAsync(source: RbxDb, key: key, value: result<data, DataStoreKeyInfo>): (data, DataStoreKeyInfo)
-  return source.datastore:UpdateAsync(tostring(key), value)
+  return wrapNull(source.datastore:UpdateAsync(key, value))
 end
 
 local RbxDb: RbxDbImpl = { } :: RbxDbImpl do
@@ -171,13 +212,13 @@ local RbxDb: RbxDbImpl = { } :: RbxDbImpl do
   end
 
   function RbxDb.SetAsync(self, key, value, uids, opt)
-    assert(not self.ordered or isInt(value), `arg #1 invalid: { ORDERED_INT_ONLY }`)
+    assert(not self.ordered or isInt(value), `arg #1 invalid: { E_ORDERED_INT_ONLY }`)
     return request(setAsync, self, key, true, value, uids, opt)
   end
 
   function RbxDb.UpdateAsync(self, key, transform)
     return request(updateAsync, self, key, true, function(value, info)
-      assert(not self.ordered or isInt(value), `arg #1 invalid: { ORDERED_INT_ONLY }`)
+      assert(not self.ordered or isInt(value), `arg #1 invalid: { E_ORDERED_INT_ONLY }`)
       transform(value, info)
     end)
   end
@@ -214,8 +255,8 @@ local RbxDb: RbxDbImpl = { } :: RbxDbImpl do
     return function(value, callback)
       resume(thread(function()
         if type(callback) == 'function' then
-          if self.ordered then
-            self.warn('OrderedDataStore does not support versioning, passing null to callback')
+          if self.ordered and __WARNINGS__ then
+            self.warn(key, W_ORDERED_SET_VERS)
           end
           callback(self:SetAsync(key, value, uids, opt))
           return
@@ -249,38 +290,52 @@ local RbxDb: RbxDbImpl = { } :: RbxDbImpl do
     __index = function(self, k)
       return setmetatable({ }, {
         __metatable = true,
-        __mode = 'kv',
+        __mode = 'v',
       })
     end,
     __metatable = true,
-    __mode = 'kv',
+    __mode = 'v',
   })
 
   function RbxDb.connect(name, scope, ordered, opt)
     assert(not ordered or not opt, 'arg #4 invalid: cannot pass DataStoreOptions to OrderedDataStore')
     local scope = scope or DEFAULT_SCOPE
-    local connection = schema[name][scope]
-    if connection then
-      return connection
-    end
-    connection = setmetatable({
+    -- is this a typecheck hack?
+    local connection: RbxDb
+    connection = schema[name][scope] or setmetatable({
       datastore = datastore(name, scope, ordered, opt),
       keys = setmetatable({ }, {
         __metatable = true,
-        __mode = 'kv',
+        __mode = 'v',
       }),
       locked = setmetatable({ }, {
         __metatable = true,
-        __mode = 'kv',
+        __mode = 'v',
       }),
       name = name,
       ordered = ordered or false,
       scope = scope,
-      print = function<A...>(...: A...)
-        print(`[RbxDb][{ connection }]`, ...)
+      -- these are probably hacks
+      print = function<A...>(head: unknown, ...: A...)
+        if select('#', ...) > 0 then
+          print(`[{ connection }][{ head }]`, ...)
+        else
+          print(`[{ connection }]`, head)
+        end
       end,
-      warn = function<A...>(...: A...)
-        warn(`[RbxDb][{ connection }]`, ...)
+      wait = function(key: key)
+        local elapsed = 0
+        while connection.locked[key] or (tick() - (connection.keys[key] or 0)) < KEY_REQUEST_THROTTLE do
+          elapsed += wait(6);
+        end
+        return elapsed
+      end,
+      warn = function<A...>(head: unknown, ...: A...)
+        if select('#', ...) > 0 then
+          warn(`[{ connection }][{ head }]`, ...)
+        else
+          warn(`[{ connection }]`, head)
+        end
       end,
     }, RbxDb)
     schema[name][scope] = connection
@@ -294,6 +349,12 @@ local RbxDb: RbxDbImpl = { } :: RbxDbImpl do
     end
     return connection
   end
+
+  game:BindToClose(function()
+    while requests > 0 do
+      wait(1)
+    end
+  end)
 end
 
 return RbxDb
